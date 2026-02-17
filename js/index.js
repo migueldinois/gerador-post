9bf5e8077c9f212961ae97840457d876caa85570
migueldinois <dinoismiguel2@gmail.com>
2026-02-17 13:09:33 -0300

    Varias eventos adicionados, falta o evento de trocar data

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const nomeExibicao = document.getElementById('nomeExibicao')
+const inputNomeExibicao = document.getElementById('inputNomeExibicao')
+
+const nomeUsuario = document.getElementById('nomeUsuario')
+const inputNomeUsuario = document.getElementById('inputNomeUsuario')
+
+
+const conteudoPost = document.getElementById('conteudoPost')
+const inputConteudoPost = document.getElementById('inputConteudoPost')
+
+const fotoDePerfil = document.getElementById('fotoDePerfil')
+const inputFotoDePerfil = document.getElementById('inputFotoDePerfil')
+
+const dataPost = document.getElementById('dataPost')
+const inputDataPost = document.getElementById('inputDataPost')
+
+
+inputNomeExibicao.addEventListener('input', () => {
+
+  nomeExibicao.textContent = inputNomeExibicao.value;
+
+});
+
+inputNomeUsuario.addEventListener('input', () => {
+
+  nomeUsuario.textContent = ("@" + inputNomeUsuario.value);
+
+});
+
+inputConteudoPost.addEventListener('input', () => {
+
+  conteudoPost.textContent = inputConteudoPost.value;
+
+});
+
+inputFotoDePerfil.addEventListener('input', () => {
+
+    fotoDePerfil.src = inputFotoDePerfil.value
+
+})
+
+inputDataPost.addEventListener('input', () => {
+
+})
